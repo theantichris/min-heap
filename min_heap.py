8fa6ccae7368b3dc244f2b9b2ef29d021675388f
Christopher Lamm <chris@theantichris.com>
2019-12-16 07:23:39 -0500

    added add() method

--- min_heap.py
+++ min_heap.py
@@ -2,3 +2,8 @@ class MinHeap:
     def __init__(self):
         self.heap_list = [None]
         self.count = 0
+
+    def add(self, element):
+        print("Adding {} to {}.".format(element, self.heap_list))
+        self.count += 1
+        self.heap_list.append(element)
--- script.py
+++ script.py
@@ -1,4 +1,7 @@
-from min_heap import MinHeap
+from min_heap import MinHeap 
 
-heap = MinHeap()
-print(heap.heap_list)
+min_heap = MinHeap()
+print(min_heap.heap_list)
+
+min_heap.add(42)
+print(min_heap.heap_list)
