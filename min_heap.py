4a27f7cba5a8783402e380b39f8bc71c6ca6018d
Christopher Lamm <chris@theantichris.com>
2019-12-16 11:27:16 -0500

    clean up

--- min_heap.py
+++ min_heap.py
@@ -4,28 +4,29 @@ class MinHeap:
         self.count = 0
 
     def add(self, element):
-        print("Adding {} to {}.".format(element, self.heap_list))
+        print("Adding {} to {}".format(element, self.heap_list))
         self.count += 1
         self.heap_list.append(element)
         self.heapify_up()
 
     def heapify_up(self):
-        print("Restoring the heap property...")
+        print("Restoring the heap...")
         idx = self.count
-        while self.parent_idx(idx) > 0:
+        parent_idx = self.get_parent_idx(idx)
+        while parent_idx > 0:
             child = self.heap_list[idx]
-            parent = self.heap_list[self.parent_idx(idx)]
+            parent = self.heap_list[parent_idx]
             if parent > child:
                 print("swapping {} with {}".format(parent, child))
                 self.heap_list[idx] = parent
-                self.heap_list[self.parent_idx(idx)] = child
-            idx = self.parent_idx(idx)
-        print("Heap Restored {}".format(self.heap_list))
+                self.heap_list[parent_idx] = child
+            idx = parent_idx
+        print("Heap Restored: {}".format(self.heap_list))
 
     def heapify_down(self):
+        print("Restoring the heap...")
         idx = 1
-        while self.child_present(idx):
-            print("Heapifying down!")
+        while self.has_child(idx):
             smaller_child_idx = self.get_smaller_child_idx(idx)
             child = self.heap_list[smaller_child_idx]
             parent = self.heap_list[idx]
@@ -35,13 +36,11 @@ class MinHeap:
             idx = smaller_child_idx
         print("Heap Restored! {}".format(self.heap_list))
 
-    def child_present(self, idx):
-        return self.left_child_idx(idx) <= self.count
-
     def retrieve_min(self):
         if self.count == 0:
             print("The heap is empty.")
             return None
+
         min = self.heap_list[1]
         self.heap_list[1] = self.heap_list[self.count]
         self.count -= 1
@@ -50,25 +49,30 @@ class MinHeap:
         self.heapify_down()
         return min
 
-    def parent_idx(self, idx):
+    def has_child(self, idx):
+        return self.get_left_child_idx(idx) <= self.count
+
+    def get_parent_idx(self, idx):
         return idx // 2
 
-    def left_child_idx(self, idx):
+    def get_left_child_idx(self, idx):
         return idx * 2
 
-    def right_child_idx(self, idx):
+    def get_right_child_idx(self, idx):
         return idx * 2 + 1
 
     def get_smaller_child_idx(self, idx):
-        if self.right_child_idx(idx) > self.count:
-            print("There is only a left child")
-            return self.left_child_idx(idx)
+        left_child_idx = self.get_left_child_idx(idx)
+        right_child_idx = self.get_right_child_idx(idx)
+        if right_child_idx > self.count:
+            print("There is only a left child.")
+            return left_child_idx
 
-        left_child = self.heap_list[self.left_child_idx(idx)]
-        right_child = self.heap_list[self.right_child_idx(idx)]
+        left_child = self.heap_list[left_child_idx]
+        right_child = self.heap_list[right_child_idx]
         if left_child < right_child:
             print("The left child is smaller.")
-            return self.left_child_idx(idx)
+            return left_child_idx
         else:
             print("The right child is smaller.")
-            return self.right_child_idx(idx)
+            return right_child_idx
