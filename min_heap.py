1800925d69d391a36d091ffaaca3955ec38fc247
Christopher Lamm <chris@theantichris.com>
2019-12-16 07:20:44 -0500

    added MinHeap class

---
+++ min_heap.py
@@ -0,0 +1,4 @@
+class MinHeap:
+    def __init__(self):
+        self.heap_list = [None]
+        self.count = 0
--- script.py
+++ script.py
@@ -1,1 +1,4 @@
-print("test")
+from min_heap import MinHeap
+
+heap = MinHeap()
+print(heap.heap_list)
